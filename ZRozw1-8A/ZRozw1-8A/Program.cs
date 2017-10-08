ce185136ddbad6a873feea2e2a957dc482442334
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:47:18 +0200

    Add files via upload

---
+++ ZRozw1-8A/ZRozw1-8A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_8A
+{
+    class Program
+    {
+        static int ObliczSume(int a, int b)
+        {
+            return a + b;
+        }
+        
+        static void Main(string[] args)
+        {
+            int a = 5, b = -7;
+            Console.WriteLine(ObliczSume(a, b));
+            Console.ReadKey();
+        }
+    }
+}
